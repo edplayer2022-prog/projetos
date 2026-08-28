1a597fd9323b7b851758ec5533574bdf4b17e42d
edplayer2022-prog <edplayer2022@gmail.com>
2026-08-28 07:35:23 -0400

    test: support Playwright web server on Windows

--- playwright.config.js
+++ playwright.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   testDir: './tests',
   use: { baseURL: 'http://127.0.0.1:4173', ...(process.env.PLAYWRIGHT_CHROME ? { launchOptions: { executablePath: process.env.PLAYWRIGHT_CHROME } } : {}) },
   webServer: {
-    command: 'python3 -m http.server 4173',
+    command: process.platform === 'win32' ? 'python -m http.server 4173' : 'python3 -m http.server 4173',
     url: 'http://127.0.0.1:4173',
     reuseExistingServer: true
   }
